824f6369a27f3e3e895ad273a1b7f2fc91706a83
EmilyLow <emlow@me.com>
2020-06-23 15:26:46 -0500

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+//Note: Had to seperately install axios with 'npm install axios --save'. See if this is a recurring problem
+import axios from 'axios';
+import NasaPage from "./components/NasaPage.js"
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  
+  useEffect(() => {
+    //axios
+    axios
+    .get("")
+    .then( res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+
+  }, []);
+  
+  
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    // <div className="App">
+    //   <p>
+    //     Read through the instructions in the README.md file to build your NASA
+    //     app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+    //   </p>
+    // </div>
+    <div>
+      <h1>Nasa Picture</h1>
+       <NasaPage />
     </div>
+    
   );
 }
 
---
+++ src/components/NasaPage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import NasaPhoto from "./NasaPhoto";
+
+const NasaPage = props => {
+    
+    
+    return (
+      <div></div>
+    );
+  };
+
+export default NasaPage;
