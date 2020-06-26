d3d575186df2f845b93061aad9c71df596dff0b6
EmilyLow <emlow@me.com>
2020-06-25 20:34:29 -0500

    Added date box

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const DateBox = styled.p`
+   height: 25px;
+   width: 200px;
+   background-color: white;
+   color: 	#696969;
+   margin-left: auto;
+   margin-right: auto;
+   margin-top: 20px;
+`;
+
+
 
 const Date = props => {
     //console.log("Props from Date", props);
 
+    
     return (
-        <p>
+        <DateBox>
             Date: {props.date}
-        </p>
+        </DateBox>
     )
     
 
